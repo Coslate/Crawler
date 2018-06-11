1448ad859d77b4a6983a4a1c232252115b15c22b
Coslate <Coslate>
2018-06-11 20:31:40 +0800

    Enhance the Connection Error Handling

--- scrape_ptt/scrape_ptt.py
+++ scrape_ptt/scrape_ptt.py
@@ -24,6 +24,7 @@ import re
 import argparse
 import datetime
 import sys
+import time
 
 #########################
 #     Main-Routine      #
@@ -82,6 +83,7 @@ def ArgumentParser(start_time):
     is_debug        = 0
     url             = "https://www.ptt.cc/bbs/nba/index.html"
     case_sensitive  = 0
+    thresh_occur    = 0
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--start_time", "-st", help="set the start time of searching articles")
@@ -128,15 +130,24 @@ def GetStrValue(tag, numeric_if_none):
     else:
         return tag.get_text().strip()
 
-def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_dic, keyword, is_debug, out_dir, case_sensitive, thresh_occur):
+def GetThePageAndUpdateURL(date_reform_prev, earlest_time, url, articles_dic_arr, start_time, end_time, month_dic, keyword, is_debug, out_dir, case_sensitive, thresh_occur):
     #--------------------------------------------------------------
     #Step1. Issue Request.
     #--------------------------------------------------------------
-    try:
-        response = requests.get(url, cookies = {'over18':"1"}, verify = False)
-    except HTTPError as e:
-        print(e)
-        sys.exit()
+    response = None
+    while(response is None):
+        try:
+            response = requests.get(url, cookies = {'over18':"1"}, verify = False)
+        except HTTPError as e:
+            print(e)
+            sys.exit()
+        except:
+            print("Connection refused by the server..")
+            print("Reconnected after 5 seconds")
+            print("...")
+            time.sleep(5)
+            print("Continuing...")
+            continue
 
     #--------------------------------------------------------------
     #Step2. Interpret Response with Beautifulsoup.
@@ -144,7 +155,6 @@ def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_di
 
     soup         = BeautifulSoup(response.text, 'lxml')
     articles     = soup.find_all('div', {"class":"r-ent"})
-    earlest_time = 99999999
 
     for article in articles:
         title_class = article.find('div', 'title').find('a') or BeautifulSoup('<a>本文已被刪除</a>', 'lxml').a
@@ -154,7 +164,7 @@ def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_di
         date     = GetStrValue(article.find('div', 'meta').find('div', {"class":"date"}), 0)
         author   = GetStrValue(article.find('div', 'meta').find('div', {"class":"author"}), 0)
 
-        if(title == "None"):
+        if((title == "None") or (re.match(r'\s*\[\s*公告\s*\].*', title) is not None)):
             continue
 
         #Get the link_url to the content
@@ -162,8 +172,49 @@ def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_di
 
         #Get the detailed timing info of the content
         date_reform = GetTimeInfo(link_url, month_dic, start_time, is_debug)
+
+        if((len(str(date_reform)) < 8) and (date_reform is not None)):
+            if(earlest_time == 99999999):
+                print(f'Warning : Get the date_reform = {date_reform} in the article -> ')
+                print(f"Warning : title_f = {title}")
+                print(f"Warning : push_num_f = {push_num}")
+                print(f"Warning : author_f = {author}")
+                print(f"Warning : date_f = {date}")
+                print(f"Warning : link_url_f = {link_url}")
+                continue
+            else:
+                time_match = re.match(r'(\d+)(\d)(\d)(\d)(\d)', date_reform_prev)
+                if(time_match is not None):
+                    month_prev = str(time_match.group(2))+str(time_match.group(3))
+                    dat_prev = str(time_match.group(4))+str(time_match.group(5))
+                    year_prev = str(time_match.group(1))
+
+                    time_match_curr = re.match(r'(\d+)/(\d+)', date)
+                    if(time_match_curr is not None):
+                        month_curr = str(time_match_curr.group(1)).zfill(2)
+                        day_curr = str(time_match_curr.group(2)).zfill(2)
+
+                        if(month_prev == month_curr):
+                            date_reform = str(year_prev)+str(month_curr)+str(day_curr)
+                        elif(int(month_curr) > int(month_prev)):
+                            date_reform = str(year_prev)+str(month_curr)+str(day_curr)
+                        else:
+                            date_reform = str((int(year_prev)-1))+str(month_curr)+str(day_curr)
+                    else:
+                        date_reform = date_reform_prev
+                        print(f'Warning : date is not match the x/y format. Please check the article -> ')
+                        print(f"Warning : title_f = {title}")
+                        print(f"Warning : push_num_f = {push_num}")
+                        print(f"Warning : author_f = {author}")
+                        print(f"Warning : date_f = {date}")
+                        print(f"Warning : link_url_f = {link_url}")
+                else:
+                    date_reform = None
+
         if(date_reform == None):
             continue
+
+        date_reform_prev = date_reform
         if(int(date_reform) < earlest_time):
             earlest_time = int(date_reform)
 
@@ -176,6 +227,14 @@ def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_di
             if(keyword == None):
                 articles_dic_arr.append({"title":title, "link":link_url, "push_num":push_num, "date":date, "author":author, "date_reform":date_reform, "content_info":content_info})
                 ProcessTitleWriteOutFile(title, date_reform, author, push_num, out_dir, content_info)
+                if(is_debug):
+                    print(f"============================")
+                    print(f"title = {title}")
+                    print(f"push_num = {push_num}")
+                    print(f"author = {author}")
+                    print(f"date = {date}")
+                    print(f"date_reform = {date_reform}")
+                    print(f"link_url = {link_url}")
 
             else:
                 str_line_arr   = content_info.split('\n')
@@ -189,8 +248,14 @@ def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_di
                     ProcessTitleWriteOutFile(title, date_reform, author, push_num, out_dir, content_info)
 
                 if(is_debug):
+                    print(f"============================")
                     print(f"l_cnt = {l_cnt}")
                     print(f"l_cnt_title = {l_cnt_title}")
+                    print(f"title = {title}")
+                    print(f"push_num = {push_num}")
+                    print(f"author = {author}")
+                    print(f"date = {date}")
+                    print(f"date_reform = {date_reform}")
 
     #--------------------------------------------------------------
     #Step3. Find the URL of Previous Pages.
@@ -198,63 +263,96 @@ def GetThePageAndUpdateURL(url, articles_dic_arr, start_time, end_time, month_di
     link_url = soup.find("div", {"class":re.compile("^btn\-group\s*btn\-group\-paging$")}).find("a", {"class":"btn wide"}, text=re.compile("上頁"))["href"]
     link_url = urlparse.urljoin(url, link_url)
 
-    return (link_url, earlest_time)
+    if(is_debug):
+        print(f'================================')
+        print(f'=====index_url = {link_url}=====')
+        print(f'================================')
+
+    return (link_url, earlest_time, date_reform_prev)
 
 def GetAllThePages(start_time, end_time, keyword, url, month_dic, is_debug, out_dir, case_sensitive, thresh_occur):
     articles_dic_arr = []
     count_once = 0
+    earlest_time = 99999999
+    date_reform_prev = 0
+
     while(True):
         this_loop_article = []
-        (url, earlest_time) = GetThePageAndUpdateURL(url, this_loop_article, start_time, end_time, month_dic, keyword, is_debug, out_dir, case_sensitive, thresh_occur)
+        (url, earlest_time, date_reform_prev) = GetThePageAndUpdateURL(date_reform_prev, earlest_time, url, this_loop_article, start_time, end_time, month_dic, keyword, is_debug, out_dir, case_sensitive, thresh_occur)
         for each_article in this_loop_article:
             articles_dic_arr.append((each_article))
         if(count_once < 2):
             count_once += 1
         if((int(earlest_time) < int(end_time)) and (count_once > 1)):
+            print(f'earlest_time <break>= {earlest_time}')
             break
 
     return (articles_dic_arr)
 
 def GetTimeInfo(link_url, month_dic, start_time, is_debug):
-    try:
-        response = requests.get(link_url, verify = False)
-    except HTTPError as e:
-        print(e)
-        sys.exit()
+    response = None
+    while(response is None):
+        try:
+            response = requests.get(link_url, verify = False)
+        except HTTPError as e:
+            print(e)
+            sys.exit()
+        except:
+            print("Connection refused by the server..")
+            print("Reconnected after 5 seconds")
+            time.sleep(5)
+            print("Continuing...")
+            continue
 
     soup = BeautifulSoup(response.text, 'lxml')
 
     try:
         time_blk = soup.find('span', 'article-meta-value', text=re.compile("^\S+\s+(\S+)\s+(\S+)\s+\S+\:\S+\:\S+\s+(\S+)\s*")).get_text()
+        time_match = re.match(r"^\S+\s*(\S+)\s*(\S+)\s*\S+\:\S+\:\S+\s*(\S+)\s*", time_blk)
+        month = month_dic[time_match.group(1)]
+        year  = int(time_match.group(3))
+        day   = "{:02d}".format(int(time_match.group(2)))
     except AttributeError as e:
-        print(e)
-        return str(start_time)
+        try:
+            time_blk = soup.find('span', 'f2', text=re.compile("※\s*編輯:\s*.*,\s*\d*/\d*/\d*\s*\S+:\S+:\S+\s*")).get_text()
+            time_match = re.match(r"※\s*編輯:\s*.*,\s*(\d+)/(\d+)/(\d+)\s*\S+:\S+:\S+\s*", time_blk)
+            month = int(time_match.group(1))
+            year  = int(time_match.group(3))
+            day   = "{:02d}".format(int(time_match.group(2)))
+        except AttributeError as e:
+            print(e)
+            print('Warning : time_blk not found.')
+            print('Warning : return None.')
+            return None
 
     if(is_debug):
         print(f'time_blk ={time_blk}')
 
-    time_match = re.match(r"^\S+\s*(\S+)\s*(\S+)\s*\S+\:\S+\:\S+\s*(\S+)\s*", time_blk)
-    month = month_dic[time_match.group(1)]
-    year  = time_match.group(3)
-    day   = "{:02d}".format(int(time_match.group(2)))
-
-    return(year+month+day)
+    return(str(year)+str(month)+str(day))
 
 def GetContentInfo(link_url):
     payload = {
         "from" : "/bbs/Gossiping/index.html",
         "yes"  : "yes"
     }
-    rs = requests.session()
-    res = rs.post("https://www.ptt.cc/ask/over18", verify = False, data = payload)
 
-    try:
-#        response = requests.get(link_url)
-        response = rs.get(link_url, verify = False)
-    except HTTPError as e:
-        print(e)
-        print('Respones None')
-        sys.exit()
+    response = None
+    while(response is None):
+        try:
+#           response = requests.get(link_url)
+            rs = requests.session()
+            res = rs.post("https://www.ptt.cc/ask/over18", verify = False, data = payload)
+            response = rs.get(link_url, verify = False)
+        except HTTPError as e:
+            print(e)
+            print('Respones None')
+            sys.exit()
+        except:
+            print("Connection refused by the server..")
+            print("Reconnected after 5 seconds")
+            time.sleep(5)
+            print("Continuing...")
+            continue
 
     soup = BeautifulSoup(response.text, 'lxml')
     content_blk = soup.find('div', class_="bbs-screen bbs-content", id='main-content', recursive=True)
